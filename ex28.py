b12709d80a4d892bc3e5173bbc2677ce866ad254
BMDACMER <1163753605@qq.com>
2018-07-06 13:26:41 +0800

    添加习题27-30  即将看P92

---
+++ ex28.py
@@ -0,0 +1,15 @@
+''''
+布尔表达式练习  P85
+'''
+
+print(True and True)
+print(False and False)
+print(1 == 1 and 2 == 1)
+print("test" == "test")
+print(1 == 1 or 2 != 1)
+print(True and 1==1)
+print(False and 0 != 0)
+print(True or 1 == 1)
+
+print("=====================")
+print(3 == 3 and not ("testing" == "testing" or "Python"=="Fun"))
